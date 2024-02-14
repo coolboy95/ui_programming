016c49ed011f56543560fa93813afe88dbb1b211
coolboy95 <nikhilkhuran@adobe.com>
2024-02-14 13:12:25 +0530

    used client to screen

--- twoDiffWindowsNonEmbedded/ui_programming.cpp
+++ twoDiffWindowsNonEmbedded/ui_programming.cpp
@@ -10,12 +10,12 @@ LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK    WndProc2(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
 	const char className1[] = "H-instance window class1";
 	const char className2[] = "H-instance window class2";
@@ -81,11 +81,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	int smallerClassW = 400;
 	int smallerClassH = 100;
 
+	
+
 	HWND hWnd = CreateWindowExA(0, className1, "New Title1", WS_OVERLAPPEDWINDOW,
 		biggerClassX, biggerClassY, biggerClassW, biggerClassH, nullptr, nullptr, hInstance, nullptr);
 
+	POINT biggerWindowBegin = {0,0};
+	ClientToScreen(hWnd, &biggerWindowBegin);
+	int NonClientAreaHeight = biggerWindowBegin.y - biggerClassY;
+	int NonClientAreaWidthOffset = biggerWindowBegin.x - biggerClassX;
+
 	HWND hWnd2 = CreateWindowExA(0, className2, "New Title2", WS_OVERLAPPEDWINDOW,
-		biggerClassX + biggerClassW - smallerClassW, biggerClassY+biggerClassH-smallerClassH, smallerClassW, smallerClassH, nullptr, nullptr, hInstance, nullptr);
+		biggerWindowBegin.x + (biggerClassW- NonClientAreaWidthOffset) - smallerClassW, biggerWindowBegin.y + (biggerClassH-NonClientAreaHeight) - smallerClassH, smallerClassW, smallerClassH, nullptr, nullptr, hInstance, nullptr);
 
 	if (!hWnd || !hWnd2)
 	{
@@ -98,20 +105,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	ShowWindow(hWnd2, TRUE);
 	UpdateWindow(hWnd2);
 
-    HACCEL hAccelTable = NULL;
+	HACCEL hAccelTable = NULL;
 
-    MSG msg;
-	
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {   
-        DispatchMessage(&msg);   
-    }
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		DispatchMessage(&msg);
+	}
 
 	UnregisterClassA(className1, hInst);
 	UnregisterClassA(className2, hInst);
-	
-    return (int) msg.wParam;
+
+	return (int)msg.wParam;
 }
 
 //
@@ -147,9 +154,9 @@ void CreatePolygon(HDC hdc)
 	p5.x = 160;
 	p5.y = 60;
 
-	POINT pArray[] = {p1, p2, p3, p4, p5};
+	POINT pArray[] = { p1, p2, p3, p4, p5 };
 
-	Polygon(hdc, pArray, sizeof(pArray)/sizeof(pArray[0]));
+	Polygon(hdc, pArray, sizeof(pArray) / sizeof(pArray[0]));
 }
 
 void CreateLine(HDC hdc)
@@ -162,7 +169,7 @@ void CreateLine(HDC hdc)
 	p2.x = 350;
 	p2.y = 100;
 
-	POINT pArray[] = {p1,p2};
+	POINT pArray[] = { p1,p2 };
 
 	HPEN hPenPurple = CreatePen(PS_SOLID, 2, RGB(255, 0, 255));
 	HPEN hPenInitial = (HPEN)SelectObject(hdc, hPenPurple);
@@ -173,67 +180,67 @@ void CreateLine(HDC hdc)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-           
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-			RECT outsideRect = {};
-			GetClientRect(hWnd, &outsideRect); // get the client area rectangle
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
 
-			HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
-			HBRUSH hBrushInitial = (HBRUSH)SelectObject(hdc, hBrushRed);
-			Rectangle(hdc, outsideRect.left, outsideRect.top, outsideRect.right, outsideRect.bottom);
-			DeleteObject(hBrushRed);
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		RECT outsideRect = {};
+		GetClientRect(hWnd, &outsideRect); // get the client area rectangle
 
-			RECT insideRect = {outsideRect.right/2-50, outsideRect.bottom/2 - 15, outsideRect.right/2 + 50, outsideRect.bottom/2 + 15}; // width 100 and height 30
-			HBRUSH hBrushBlue = CreateSolidBrush(RGB(0, 0, 255));
-			SelectObject(hdc, hBrushBlue);
+		HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
+		HBRUSH hBrushInitial = (HBRUSH)SelectObject(hdc, hBrushRed);
+		Rectangle(hdc, outsideRect.left, outsideRect.top, outsideRect.right, outsideRect.bottom);
+		DeleteObject(hBrushRed);
 
-			HPEN hPenYellow = CreatePen(PS_SOLID, 5, RGB(255, 255, 0));
-			HPEN hPenInitial = (HPEN)SelectObject(hdc, hPenYellow);
+		RECT insideRect = { outsideRect.right / 2 - 50, outsideRect.bottom / 2 - 15, outsideRect.right / 2 + 50, outsideRect.bottom / 2 + 15 }; // width 100 and height 30
+		HBRUSH hBrushBlue = CreateSolidBrush(RGB(0, 0, 255));
+		SelectObject(hdc, hBrushBlue);
 
-			SetBkMode(hdc, TRANSPARENT);
-			HFONT newFont = CreateFontA(14, 0, 0, 0, 700, 0, 0, 0, ANSI_CHARSET, OUT_CHARACTER_PRECIS, CLIP_DEFAULT_PRECIS,
-				DEFAULT_QUALITY, DEFAULT_PITCH, "Times New Roman");
-			HFONT initialFont = (HFONT)SelectObject(hdc, newFont);
+		HPEN hPenYellow = CreatePen(PS_SOLID, 5, RGB(255, 255, 0));
+		HPEN hPenInitial = (HPEN)SelectObject(hdc, hPenYellow);
 
-			Rectangle(hdc, insideRect.left, insideRect.top, insideRect.right, insideRect.bottom);
-			
-			SetTextColor(hdc, RGB(0,0,0)); // Setting black color
-			DrawTextA(hdc, "YOLO", -1, &insideRect, DT_CENTER);
+		SetBkMode(hdc, TRANSPARENT);
+		HFONT newFont = CreateFontA(14, 0, 0, 0, 700, 0, 0, 0, ANSI_CHARSET, OUT_CHARACTER_PRECIS, CLIP_DEFAULT_PRECIS,
+			DEFAULT_QUALITY, DEFAULT_PITCH, "Times New Roman");
+		HFONT initialFont = (HFONT)SelectObject(hdc, newFont);
 
-			SelectObject(hdc, initialFont);
-			DeleteObject(newFont);
+		Rectangle(hdc, insideRect.left, insideRect.top, insideRect.right, insideRect.bottom);
 
-			SelectObject(hdc, hPenInitial);
-			DeleteObject(hPenYellow);
+		SetTextColor(hdc, RGB(0, 0, 0)); // Setting black color
+		DrawTextA(hdc, "YOLO", -1, &insideRect, DT_CENTER);
 
-			SelectObject(hdc, hBrushInitial);
-			DeleteObject(hBrushBlue);
+		SelectObject(hdc, initialFont);
+		DeleteObject(newFont);
 
-			CreatePolygon(hdc);
-			CreateLine(hdc);
+		SelectObject(hdc, hPenInitial);
+		DeleteObject(hPenYellow);
 
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
+		SelectObject(hdc, hBrushInitial);
+		DeleteObject(hBrushBlue);
+
+		CreatePolygon(hdc);
+		CreateLine(hdc);
+
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
 	{
 		OutputDebugStringA("Win proc 1 WM_Destroy\n");
 		PostQuitMessage(0);
 	}
-    break;
-    default:
-        return DefWindowProcA(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	break;
+	default:
+		return DefWindowProcA(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
 LRESULT CALLBACK WndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
