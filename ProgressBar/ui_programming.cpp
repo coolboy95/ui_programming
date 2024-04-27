c5af6506f32ce84feca4d119925ca894949eeba5
coolboy95 <nikhilkhuran@adobe.com>
2024-04-27 16:43:55 +0530

    Progress bar done

--- ProgressBar/ui_programming.cpp
+++ ProgressBar/ui_programming.cpp
@@ -5,7 +5,10 @@
 #include "ui_programming.h"
 #include "stdio.h"
 #include "time.h"
+#include "thread"
 
+#define UPDATEPROGRESSID 202
+#define UPDATEPROGRESSNC 302
 #define biggerClassX 100
 #define biggerClassY 200
 #define biggerClassW 1000
@@ -27,25 +30,12 @@ void log(const char* str)
 
 
 HINSTANCE hInst;
-const UINT timer_id = 10;
+HWND hWnd2;
+HWND hWnd;
+double rightPercentageChildWindow;
 
 LRESULT CALLBACK    WndProcMainWindow(HWND, UINT, WPARAM, LPARAM);
 LRESULT CALLBACK    WndProcChildWindow(HWND, UINT, WPARAM, LPARAM);
-
-VOID __stdcall MyTimerProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
-{
-	time_t rawtime;
-	struct tm* timeinfo;
-	char buffer[80];
-	if (WM_TIMER == uMsg) {
-		if (idEvent == timer_id) {
-			time(&rawtime);
-			timeinfo = localtime(&rawtime);
-			strftime(buffer, 80, "Now time is %I:%M:%S%p.", timeinfo);
-			SetWindowTextA(hWnd, buffer);
-		}
-	}
-}
 
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -58,6 +48,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	const char className1[] = "H-instance window class1";
 	const char className2[] = "H-instance window class2";
+
+	rightPercentageChildWindow = 0.1;
 
 	WNDCLASSEXA wcex = {};
 
@@ -111,7 +103,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	hInst = hInstance;
 
-	HWND hWnd = CreateWindowExA(0, className1, "New Title1", WS_OVERLAPPEDWINDOW,
+	hWnd = CreateWindowExA(0, className1, "New Title1", WS_OVERLAPPEDWINDOW,
 		biggerClassX, biggerClassY, biggerClassW, biggerClassH, nullptr, nullptr, hInstance, nullptr);
 
 	POINT biggerWindowBegin = { 0,0 };
@@ -119,9 +111,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	int NonClientAreaHeight = biggerWindowBegin.y - biggerClassY;
 	int NonClientAreaWidthOffset = biggerWindowBegin.x - biggerClassX;
 
-	HWND hWnd2 = CreateWindowExA(0, className2, "New Title2", WS_CHILD,
+	hWnd2 = CreateWindowExA(0, className2, "New Title2", WS_CHILD,
 		(biggerClassW - NonClientAreaWidthOffset) - smallerClassW, (biggerClassH - NonClientAreaHeight) - smallerClassH, smallerClassW, smallerClassH, hWnd, nullptr, hInstance, nullptr);
-	
+
 	if (!hWnd || !hWnd2)
 	{
 		return FALSE;
@@ -133,11 +125,27 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	ShowWindow(hWnd2, TRUE);
 	UpdateWindow(hWnd2);
 
-	SetTimer(hWnd, timer_id, 1000, (TIMERPROC)MyTimerProc);
-
 	HACCEL hAccelTable = NULL;
 
 	MSG msg;
+
+	std::thread t([]() {
+		
+		while (1)
+		{
+			if (rightPercentageChildWindow >= 1.0)
+			{
+				break;
+			}
+			rightPercentageChildWindow += 0.1;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			WPARAM wParam = MAKEWPARAM(UPDATEPROGRESSID, UPDATEPROGRESSNC);
+			SendMessageA(hWnd, WM_COMMAND, wParam, NULL);
+		}
+		
+	});
+	
+	
 
 	// Main message loop:
 	while (GetMessage(&msg, nullptr, 0, 0))
@@ -145,9 +153,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		DispatchMessage(&msg);
 	}
 
+	t.join();
+
 	UnregisterClassA(className1, hInst);
 	UnregisterClassA(className2, hInst);
-	KillTimer(hWnd, timer_id);
 
 	return (int)msg.wParam;
 }
@@ -169,14 +178,16 @@ LRESULT CALLBACK WndProcMainWindow(HWND hWnd, UINT message, WPARAM wParam, LPARA
 	{
 	case WM_COMMAND:
 	{
-
-	}
-	break;
-	case WM_TIMER:
-	{
-		case timer_id:
+		WORD id = loword(wParam);
+		WORD notificationCode = hiword(wParam);
+		char str[256];
+		sprintf_s(str, "MainWindow1 WM COMMAND id: %d notificationCode %d ", id, notificationCode);
+		log(str);
+		if (notificationCode == UPDATEPROGRESSNC)
 		{
-			log("timmer event received");
+			log("MainWindow1 Update progress NC received");
+			//UpdateWindow(hWnd2);
+			InvalidateRect(hWnd2, NULL, true);
 		}
 	}
 	break;
@@ -317,8 +328,21 @@ LRESULT CALLBACK WndProcChildWindow(HWND hWnd, UINT message, WPARAM wParam, LPAR
 
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
-		RECT outsideRect = {};
-		GetClientRect(hWnd, &outsideRect); // get the client area rectangle
+		RECT originalRect = {40,20,250,40};
+		RECT progressRect = { 40,20,250,40 };
+		//GetClientRect(hWnd, &outsideRect); // get the client area rectangle
+
+		HBRUSH hBrushGreen = CreateSolidBrush(RGB(0, 255, 0));
+		HBRUSH hBrushInitial = (HBRUSH)SelectObject(hdc, hBrushGreen);
+		Rectangle(hdc, originalRect.left, originalRect.top, originalRect.right, originalRect.bottom);
+		SelectObject(hdc, hBrushInitial);
+		DeleteObject(hBrushGreen);
+
+		HBRUSH hBrushBlue = CreateSolidBrush(RGB(0, 0, 255));
+		hBrushInitial = (HBRUSH)SelectObject(hdc, hBrushBlue);
+		Rectangle(hdc, progressRect.left, progressRect.top, progressRect.left + (progressRect.right-progressRect.left)*rightPercentageChildWindow, progressRect.bottom);
+		SelectObject(hdc, hBrushInitial);
+		DeleteObject(hBrushBlue);
 
 		EndPaint(hWnd, &ps);
 	}
